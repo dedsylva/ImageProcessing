9afd58a5bf93c58e1ea1aa18cb6936786fc5d22d
Dedsylva <dedsylva@gmail.com>
2021-07-05 12:53:11 -0300

    no more deds library for u

--- Deds/optimizers.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def SGD(model, gradients):
-	for i in range(len(model)):
-		model[i][0] -= gradients[-1-i][0] #updating weights
-		model[i][1] -= gradients[-1-i][1] #updating biases
-
-	return model
